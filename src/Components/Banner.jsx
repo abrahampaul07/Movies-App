de2e407620ba2493b4f965546143e121ec45c8b3
Abraham Paul <abrahamise.rymec@gmail.com>
2024-02-27 19:43:44 +0530

    banner commit

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Banner() {
-    return (
-        <div className="flex justify-center items-end h-[70vh] bg-[url('https://cdn.europosters.eu/image/hp/66923.jpg')] bg-cover bg-no-repeat">
-            <div className='text-white py-1 bg-slate-950/50 w-full text-center'>
-                Batman
-            </div>
-        </div>
-    )
+  const [movieObj, setMovieObj] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/trending/movie/day?api_key=b379b6c371be86da8a049d0a8fa2e7eb"
+      )
+      .then(function (response) {
+        // handle success
+        // console.log(response);
+        let movies = response.data.results;
+        console.log(movies);
+        let randomMovie = movies[Math.floor(Math.random() * 20)];
+        console.log(randomMovie);
+        setMovieObj(randomMovie);
+      });
+  }, []);
+
+  if (movieObj.backdrop_path === undefined) {
+    return <>...Loading</>
+  }
+
+  return (
+    <div
+      className="h-[70vh] bg-cover bg-center flex items-end "
+      style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${movieObj.backdrop_path})`}}
+    >
+      <div className="text-white py-1 bg-slate-950/50 w-full text-center">
+        {movieObj.title}
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
--- src/Components/TrendingMovies.jsx
+++ src/Components/TrendingMovies.jsx
@@ -5,12 +5,14 @@ function TrendingMovies() {
     <>
         <div className='text-center text-lg font-bold my-5'>Trending movies</div>
         <div className='flex flex-wrap justify-evenly gap-4 m-4'>
-        <div className="flex justify-center items-end h-48 w-32 rounded-lg overflow-hidden bg-[url('https://cdn.europosters.eu/image/hp/66923.jpg')] bg-cover bg-no-repeat">
-            <div className='text-sm text-white py-1 bg-slate-950/50 w-full text-center'>
+        <div className="flex justify-center items-end h-64 w-48 rounded-lg overflow-hidden bg-[url('https://cdn.europosters.eu/image/hp/66923.jpg')] bg-cover bg-no-repeat">
+          {/* <div>&#128525;</div> */}
+            <div className='text-xl text-white py-1 bg-slate-950/50 w-full text-center'>
                 Batman
             </div>
         </div>
         </div>
+        
     </>
   )
 }
