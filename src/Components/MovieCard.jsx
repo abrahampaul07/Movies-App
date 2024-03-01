5da04f95c629cb0b92d4c61aa75cb637fe9051ce
Abraham Paul <abrahamise.rymec@gmail.com>
2024-03-01 22:14:52 +0530

    handling adding and removing from watchlist

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 
-function MovieCard({title, poster_path}) {
-
+function MovieCard({
+  id,
+  title,
+  poster_path,
+  watchList,
+  handleAddToWatchList,
+  handleRemoveFromWatchList,
+}) {
   return (
-    <div className="flex flex-col justify-between hover:scale-[0.9] duration-300 justify-center items-end h-64 w-48 rounded-lg overflow-hidden bg-cover bg-no-repeat"
-    style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${poster_path})`}}
+    <div
+      className="flex flex-col justify-between hover:scale-[0.9] duration-300 justify-center items-end h-64 w-48 rounded-lg overflow-hidden bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${poster_path})`
+      }}
     >
-      {/* <div className="text-xl m-2 rounded-lg py-1 bg-slate-950/50 cursor-pointer">&#128525;</div> */}
-      <div className="text-xl m-2 rounded-lg py-1 bg-slate-950/50 cursor-pointer">&#10060;</div>
+      {watchList.includes(id) ? (
+        <div onClick={() => handleRemoveFromWatchList(id)} className="text-xl m-2 rounded-lg py-1 bg-slate-950/50 cursor-pointer">
+          &#10060;
+        </div>
+      ) : (
+        <div
+          onClick={() => handleAddToWatchList(id)}
+          className="text-xl m-2 rounded-lg py-1 bg-slate-950/50 cursor-pointer"
+        >
+          &#128525;
+        </div>
+      )}
+
       <div className="text-xl text-white py-1 bg-slate-950/50 w-full text-center">
         {title}
       </div>
--- src/Components/TrendingMovies.jsx
+++ src/Components/TrendingMovies.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import Pagination from './Pagination'
-import MovieCard from './MovieCard'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from "react";
+import Pagination from "./Pagination";
+import MovieCard from "./MovieCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function TrendingMovies() {
-
   const [movies, setMovies] = useState([]);
   const [pageNo, setPageNo] = useState(1);
+  const [watchList, setWatchList] = useState([]);
+
+  const handleAddToWatchList = (id) => {
+    const newWatchList = [...watchList];
+    newWatchList.push(id);
+    console.log(newWatchList);
+    setWatchList(newWatchList);
+  };
+
+  const handleRemoveFromWatchList = (id) => {
+    const newWatchList = watchList.filter((movieId)=> {
+      return id!==movieId;
+    })
+    setWatchList(newWatchList);
+  };
 
   const handlePrev = function () {
     if (pageNo > 1) {
@@ -19,36 +33,49 @@ function TrendingMovies() {
     setPageNo(pageNo + 1);
   };
 
-  console.log(pageNo);
+  // console.log(pageNo);
 
-  useEffect(()=> {
-    axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=b379b6c371be86da8a049d0a8fa2e7eb&page=${pageNo}`)
-    .then(function(response) {
-      setMovies(response.data.results);
-    })
-  },[pageNo]) // it will make sure to execute callback in mounting as well as updation of pageNo state
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=b379b6c371be86da8a049d0a8fa2e7eb&page=${pageNo}`
+      )
+      .then(function (response) {
+        setMovies(response.data.results);
+      });
+  }, [pageNo]); // it will make sure to execute callback in mounting as well as updation of pageNo state
 
   // console.log(movies);
 
   if (movies.length === 0) {
-    return <>...Loading</>
+    return <>...Loading</>;
   }
 
   return (
     <>
-        <div className='text-center text-lg font-bold my-5'>Trending movies</div>
-        <div className='flex flex-wrap justify-evenly gap-4 m-4'>
-          {
-            movies.map((moviesObj)=> {
-              return <MovieCard key={moviesObj.id} title={moviesObj.title} poster_path={moviesObj.poster_path}/>
-            })
-          }
-        </div>
-        <Pagination pageNo={pageNo}
-                    handlePrev={handlePrev}
-                    handleNext={handleNext}/>
+      <div className="text-center text-lg font-bold my-5">Trending movies</div>
+      <div className="flex flex-wrap justify-evenly gap-4 m-4">
+        {movies.map((moviesObj) => {
+          return (
+            <MovieCard
+              key={moviesObj.id}
+              id={moviesObj.id}
+              title={moviesObj.title}
+              poster_path={moviesObj.poster_path}
+              watchList={watchList}
+              handleAddToWatchList={handleAddToWatchList}
+              handleRemoveFromWatchList={handleRemoveFromWatchList}
+            />
+          );
+        })}
+      </div>
+      <Pagination
+        pageNo={pageNo}
+        handlePrev={handlePrev}
+        handleNext={handleNext}
+      />
     </>
-  )
+  );
 }
 
-export default TrendingMovies
+export default TrendingMovies;
