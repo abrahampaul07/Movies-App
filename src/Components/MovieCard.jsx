bf88076eeb7f5c317ff7ca2a209be15e04502557
Abraham Paul <abrahamise.rymec@gmail.com>
2024-03-01 21:23:47 +0530

    added fav and remove icon to the card

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -3,10 +3,11 @@ import React from "react";
 function MovieCard({title, poster_path}) {
 
   return (
-    <div className="flex justify-center items-end h-64 w-48 rounded-lg overflow-hidden bg-cover bg-no-repeat"
+    <div className="flex flex-col justify-between hover:scale-[0.9] duration-300 justify-center items-end h-64 w-48 rounded-lg overflow-hidden bg-cover bg-no-repeat"
     style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${poster_path})`}}
     >
-      {/* <div>&#128525;</div> */}
+      {/* <div className="text-xl m-2 rounded-lg py-1 bg-slate-950/50 cursor-pointer">&#128525;</div> */}
+      <div className="text-xl m-2 rounded-lg py-1 bg-slate-950/50 cursor-pointer">&#10060;</div>
       <div className="text-xl text-white py-1 bg-slate-950/50 w-full text-center">
         {title}
       </div>
