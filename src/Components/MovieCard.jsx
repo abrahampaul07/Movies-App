4b47ab97acc0236557e0e3c331328141544884dc
Abraham Paul <abrahamise.rymec@gmail.com>
2024-03-08 21:14:45 +0530

    responsive movie card for mobile devices

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -19,7 +19,7 @@ function MovieCard({
 
   return (
     <div
-      className="flex flex-col justify-between hover:scale-[0.9] duration-300 justify-center items-end h-64 w-48 sm:w-64 rounded-lg overflow-hidden bg-cover bg-no-repeat"
+      className="flex flex-col justify-between hover:scale-[0.9] duration-300 justify-center items-end h-64 w-36 md:w-1/2 lg:w-1/4 xl:w-1/6 rounded-lg overflow-hidden bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url(https://image.tmdb.org/t/p/original${poster_path})`,
       }}
@@ -29,7 +29,7 @@ function MovieCard({
           onClick={() => handleRemoveFromWatchList(movieObj)}
           className="text-xl m-2 rounded-lg py-1 bg-slate-950/50 cursor-pointer"
         >
-          &#10060;
+          &#10060;  
         </div>
       ) : (
         <div
